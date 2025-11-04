6c485e402cd6c5ca34ce43cd0f5fde0bf41f96b3
corbalan-nicolas <nicolas.lcorbalan@davinci.edu.ar>
2025-11-04 17:35:46 -0300

    Add tailwindcss

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,8 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 
 import './index.css'
 import {RouterProvider} from "react-router/dom";
 import router from './router.js'
-import {Outlet} from "react-router";
 
 createRoot(document.getElementById('root')).render(
 
