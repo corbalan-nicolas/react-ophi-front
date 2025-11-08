1fe157c77c1bbfe5a7db373c5c80c678dca27c3d
corbalan-nicolas <nicolas.lcorbalan@davinci.edu.ar>
2025-11-08 16:32:51 -0300

    Preparar el entorno para poder hacer peticiones a la API
    Por el momento no tuve en cuenta lo que es el JWT, pero cuando lo implementemos en la API vamos a tener que modificar algunas cositas chiquitas

---
+++ src/services/auth.js
@@ -0,0 +1,32 @@
+/*
+* ¿CÓMO HACEMOS PETICIONES A LA API?
+* Esto no es tan complejo como Laravel con Sanctum por suerte,
+* así que acá les dejo el paso a paso:
+* 1. Tener la API conectada a la DB de la nube
+* 2. Tener la API corriendo y escuchando en el puerto que hayan elegido (npm start)
+* 3. Tener configurado el CORS (supongo que voy a hacer una pull request para eso)
+*
+* Con esto ya podemos hacer peticiones al back, puede que cambie algo con el tema
+* de JST, pero no creo que haya tanta diferencia
+*
+* También, podemos usar axios si quieren (como hicimos con ophi), no lo hice solo
+* porque a lo mejor alguno quería practicar o saber cómo sería con un fetch normal.
+*
+* Si quieren implementarlo es superfácil, acá les dejo la documentación:
+* https://axios-http.com/es/docs/intro
+* */
+
+// TODO: Mover este "prefix" a una variable de entorno
+const prefix = 'http://localhost:3000/api' // Ustedes pongan el puerto que tengan en sus API's
+const endpoint = prefix + '/users'
+
+/**
+ * Obtiene todos los usuarios de la DB
+ * @returns {Promise<*>}
+ */
+export async function getAllUsers() {
+    const response = await fetch(endpoint, {})
+    const result = await response.json()
+
+    return result.data
+}
--- src/views/HomeView.jsx
+++ src/views/HomeView.jsx
@@ -1,9 +1,18 @@
+import {getAllUsers} from "../services/auth.js";
 
 
 const HomeView = () => {
+
+    async function handleClick() {
+        const users = await getAllUsers()
+        console.log(users)
+    }
+
     return (
         <>
             <h1>Inicio</h1>
+            {/* Solo para probar el servicio y la petición, esto lo voy a eliminar después lógicamente */}
+            <button onClick={handleClick}>Traer a todos los usuarios</button>
         </>
     )
 }
