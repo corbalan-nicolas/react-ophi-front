7cad9cb2d826eddfb0828595af26e2d012b5f956
corbalan-nicolas <nicolas.lcorbalan@davinci.edu.ar>
2025-11-18 19:31:19 -0300

    Hace pequeños cambios para que el profe pueda ver todo

--- src/components/MainNavigation.jsx
+++ src/components/MainNavigation.jsx
@@ -12,10 +12,27 @@ const MainNavigation = () => {
                     <li>
                         <NavLink className={'block py-2 px-4 rounded-lg bg-white/20 hover:bg-white/40 [.active]:bg-black/60 [.active]:hover:bg-black/50 [.active]:active:bg-black/70 [.active]:text-white/80 active:bg-white/30 transition'} to="/">Inicio</NavLink>
                     </li>
-                    { user.name ?
-                        <li>
-                            <NavLink className={'block py-2 px-4 rounded-lg bg-white/20 hover:bg-white/40 [.active]:bg-black/60 [.active]:hover:bg-black/50 [.active]:active:bg-black/70 [.active]:text-white/80 active:bg-white/30 transition'} to="/perfil">{user.name}</NavLink>
-                        </li>
+                    <li>
+                        <NavLink className={'block py-2 px-4 rounded-lg bg-white/20 hover:bg-white/40 [.active]:bg-black/60 [.active]:hover:bg-black/50 [.active]:active:bg-black/70 [.active]:text-white/80 active:bg-white/30 transition'} to="/perfil">{user.name}</NavLink>
+                    </li>
+                    <li>
+                        <button className={'block py-2 px-4 rounded-lg bg-white/20 hover:bg-white/40 [.active]:bg-black/60 [.active]:hover:bg-black/50 [.active]:active:bg-black/70 [.active]:text-white/80 active:bg-white/30 transition'}>Logout</button>
+                    </li>
+                    <li>
+                        <NavLink className={'block py-2 px-4 rounded-lg bg-white/20 hover:bg-white/40 [.active]:bg-black/60 [.active]:hover:bg-black/50 [.active]:active:bg-black/70 [.active]:text-white/80 active:bg-white/30 transition'} to="/iniciar-sesion">Iniciar sesión</NavLink>
+                    </li>
+                    <li>
+                        <NavLink className={"block py-2 px-4 rounded-lg bg-white/20 hover:bg-white/40 [.active]:bg-black/60 [.active]:hover:bg-black/50 [.active]:active:bg-black/70 [.active]:text-white/80 active:bg-white/30 transition"} to="/registrarse">Registrarse</NavLink>
+                    </li>
+                    {/* { user.name ?
+                        <>
+                            <li>
+                                <NavLink className={'block py-2 px-4 rounded-lg bg-white/20 hover:bg-white/40 [.active]:bg-black/60 [.active]:hover:bg-black/50 [.active]:active:bg-black/70 [.active]:text-white/80 active:bg-white/30 transition'} to="/perfil">{user.name}</NavLink>
+                            </li>
+                            <li>
+                                <button className={'block py-2 px-4 rounded-lg bg-white/20 hover:bg-white/40 [.active]:bg-black/60 [.active]:hover:bg-black/50 [.active]:active:bg-black/70 [.active]:text-white/80 active:bg-white/30 transition'}>Logout</button>
+                            </li>
+                        </>
                     :
                         <>
                             <li>
@@ -25,7 +42,7 @@ const MainNavigation = () => {
                                 <NavLink className={"block py-2 px-4 rounded-lg bg-white/20 hover:bg-white/40 [.active]:bg-black/60 [.active]:hover:bg-black/50 [.active]:active:bg-black/70 [.active]:text-white/80 active:bg-white/30 transition"} to="/registrarse">Registrarse</NavLink>
                             </li>
                         </>
-                    }
+                    } */}
 
                     {/* TODO: Panel de accesibilidad (más contraste, menos animaciones, background plano, lo que sea) */}
                 </ul>
