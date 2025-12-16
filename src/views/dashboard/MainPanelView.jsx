a93843ce07e3bef13e5c0d8a1324081b55e4012c
corbalan-nicolas <nicolas.lcorbalan@davinci.edu.ar>
2025-12-16 19:41:16 -0300

    Hace las cards responsive

--- src/views/dashboard/MainPanelView.jsx
+++ src/views/dashboard/MainPanelView.jsx
@@ -9,8 +9,8 @@ const MainPanelView = () => {
 
             <h2 className="text-4xl mb-6">¿Que deseás administrar?</h2>
             
-            <div className="flex gap-4 text-white">
-                <NavLink to="/dashboard/productos" className="bg-amber-950 w-80 border-2 border-gray-600 rounded-2xl p-5 text-2xl flex flex-col transition-all duration-200 ease-out hover:-translate-y-1 hover:shadow-xl hover:bg-amber-900 hover:border-white">
+            <div className="grid sm:grid-cols-2 xl:grid-cols-3 gap-4 text-white">
+                <NavLink to="/dashboard/productos" className="bg-amber-950 border-2 border-gray-600 rounded-2xl p-5 text-2xl flex flex-col transition-all duration-200 ease-out hover:-translate-y-1 hover:shadow-xl hover:bg-amber-900 hover:border-white">
                     <div className="flex items-center justify-between mb-4">
                         <h3 className="text-2xl font-semibold text-right">
                             Productos
@@ -23,7 +23,7 @@ const MainPanelView = () => {
                     </p>
                 </NavLink>
 
-                <NavLink to="/dashboard/usuarios" className="bg-amber-950 w-80 border-2 border-gray-600 rounded-2xl p-5 text-2xl flex flex-col transition-all duration-200 ease-out hover:-translate-y-1 hover:shadow-xl hover:bg-amber-900 hover:border-white">
+                <NavLink to="/dashboard/usuarios" className="bg-amber-950 border-2 border-gray-600 rounded-2xl p-5 text-2xl flex flex-col transition-all duration-200 ease-out hover:-translate-y-1 hover:shadow-xl hover:bg-amber-900 hover:border-white">
                     <div className="flex items-center justify-between mb-4">
                         <h3 className="text-2xl font-semibold text-right">
                             Usuarios
